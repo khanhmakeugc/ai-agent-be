7cdcc72018e862c3fb4d50e1870376aecca46718
Lautaro Nievas <lautynievas09@gmail.com>
2025-06-18 08:28:22 -0300

    browser command add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import puppeteer from 'puppeteer';
 import cors from 'cors';
 import axios from 'axios';
 import FormData from 'form-data'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
